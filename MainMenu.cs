3f7420329969c0d281a8758afbae097a59ddd26c
Sakurai223 <sofiiapetunova@gmail.com>
2025-10-15 19:26:34 +0200

    0.01V
    Dodanie Background i przycisków

---
+++ MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Show setting");
+        
+    }
+
+    public void ShowAbout()
+    {
+        Debug.Log("Show info about game");
+       
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Leave Game");
+        Application.Quit();
+    }
+}
